c8bbf09f68f2d171b035c930a0582dd87cfb5753
embyr333 <emb333@gmail.com>
2026-06-09 20:35:14 +0100

    2nd commit (Snapshot2)

--- divvier3.py
+++ divvier3.py
@@ -1,38 +1,66 @@
 '''
-Snapshot1: Coming across approach for making supersets 
-
-After quickly reading NeetCode 'Intuition' description of his 
-2nd solution (no-tree-as-such) iteration approach I realises this 
-at least might be doable for me without further assistance.
-Horrified I hadn't though of it myself!
-His code is more concise though - single-loop-single-line!
+Snapshot2: 
+Realised can use iterative superset generation approach to
+make a collection of subcollections that do include replicates.
+It will, however, have replicate subcollections, but though 
+inefficient I think it will at least allow me to make 
+a definitive split for the 'Divvier' problem, to replace
+the original tool which used random sampling to process
+larger arrays, so could not guarentee an optimal split.
+TODO: Tidy up, make an array -> best split function, GII.
 '''
-
-# from copy import deepcopy
-def subsetsMine(nums: list[int]) -> list[list[int]]:
-    coll = [[]] # collection of subsets so far
-    for num in nums:
-        # new = deepcopy(coll)
-        # ...or can do...
-        new = [sublist[:] for sublist in coll]
-        # ...or slight variant, seen afterwards...
-        # new = [list(sublist) for sublist in coll]
-        for i in range(len(new)):
-            new[i].append(num)
-        coll.extend(new)
-    return coll
-
-print(subsetsMine([1,2,3]), '\n') # [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
-print(subsetsMine([7]), '\n') # [[],[7]]
-
-
-def subsetsNC(nums: list[int]) -> list[list[int]]:
-    res = [[]]
+def subcolls_gen(nums: list[int]) -> list[list[int]]:
+    subcolls: list[list[int]] = [[]]
 
     for num in nums:
-        res += [subset + [num] for subset in res]
+        subcolls += [subcoll + [num] for subcoll in subcolls]
 
-    return res
+    return subcolls
 
-print(subsetsNC([1,2,3]), '\n') # [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
-print(subsetsNC([7]), '\n') # [[],[7]]
+# print(subcolls_gen([1,2,3])) # temp check
+# print(subcolls_gen([3,3,7,5,9])) # temp check
+
+input = [3,3,7,5,9]
+
+total = sum(input)
+is_even = not total % 2
+optimal_split = [total // 2] if is_even else [total // 2, total // 2 + 1]
+print(optimal_split) # [13, 14]
+
+subcolls = subcolls_gen(input) # TODO consider whether to remove the first, empty, item
+print('subcolls', subcolls)
+
+subcoll_sums = []
+for subcoll in subcolls:
+    subcoll_sums.append(sum(subcoll))
+print('subcoll_sums', subcoll_sums)
+
+half = total / 2
+print('half', half) # half 13.5
+min_offset = total # (or anything larger than half)
+ioffmo = 0 # index of (first found) minimum offset (from half)
+
+# for i, subcoll in enumerate(subcolls):
+#     # print('sum(subcoll) - half', sum(subcoll) - half)
+#     if abs(sum(subcoll) - half) < min_offset:
+#         min_offset = abs(sum(subcoll) - half)
+#         ioffm = i
+#     # print('min_offset', min_offset)
+# ...use the sums as will use again below
+for i, subcoll_sum in enumerate(subcoll_sums):
+    if abs(subcoll_sum - half) < min_offset:
+        min_offset = abs(subcoll_sum - half)
+        ioffm = i
+
+print('min_offset is', min_offset, 'for subcoll', subcolls[ioffm])
+# min_offset is 0.5 for subcoll [3, 3, 7]
+
+# Now find a 'complement', a subset whose sum 
+# plus that of subcolls[ioffm] add to sum(input)
+ioffc = 0 # index of (first found) complement
+for i, subcoll_sum in enumerate(subcoll_sums):
+    if sum(subcolls[ioffm]) + subcoll_sum == total:
+        ioffc = i
+        break
+print('complement is', subcolls[ioffc]) 
+# min_offset is 0.5 for subcoll [3, 3, 7]
